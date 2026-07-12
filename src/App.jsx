9ed13a42071563b7abc6aa40ea931320a097f958
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-12 13:47:00 +0530

    Created Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Search from "./components/Search";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Search />
     </>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+function Search() {
+  return (
+    <section>
+      <h2>Find Coding Challenges</h2>
+
+      <input
+        type="text"
+        placeholder="Search problems..."
+      />
+    </section>
+  );
+}
+
+export default Search;
