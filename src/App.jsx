071e58d2512084e033bae78f55c9818d76fa1353
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-12 14:10:13 +0530

    Created Companies, Contest and Testimonials components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Featured from "./components/Featured";
 import Learning from "./components/Learning";
 import Contest from "./components/Contest";
 import Companies from "./components/Companies";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Learning />
       <Companies />
        <Contest />
+       <Testimonials />
     </>
   );
 }
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,19 @@
+function Testimonials() {
+  return (
+    <section>
+      <h2>Testimonials</h2>
+
+      <div>
+        <p>"This platform helped me improve my coding skills."</p>
+        <h4>- Rahul</h4>
+      </div>
+
+      <div>
+        <p>"Weekly contests are very useful."</p>
+        <h4>- Priya</h4>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
