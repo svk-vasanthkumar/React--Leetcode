5946bc65dd0ca4edb87d4a969153e948bfa5e7f3
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-12 13:46:21 +0530

    Created Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Master Coding. Build Your Future.</h1>
+
+      <p>
+        Practice coding problems, improve your skills,
+        and prepare for technical interviews.
+      </p>
+
+      <button>Start Solving</button>
+      <button>Explore Problems</button>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>LeetCode Clone</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Problems</li>
+        <li>Contest</li>
+        <li>Learning</li>
+        <li>About</li>
+      </ul>
+
+      <div>
+        <button>Sign In</button>
+        <button>Register</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
